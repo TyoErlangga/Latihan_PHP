bdbfdfc8c600b7d58d5525f8346bff6b9948e0a1
tyoerlangga <aerlgga@gmail.com>
2025-10-01 13:43:07 +0700

    Latihan_PHP

---
+++ fungsi_perkalian.php
@@ -0,0 +1,16 @@
+<?php
+// Pembuatan fungsi perkalian
+function perkalian($angka1, $angka2)
+{
+    $a = $angka1;
+    $b = $angka2;
+    $hasil = $a * $b;
+    return $hasil; // mengembalikan nilai hasil perkalian
+}
+
+// Pemanggilan fungsi
+$hasil = perkalian(4, 5);
+echo "Perkalian 4 x 5 adalah $hasil";
+echo "<br>";
+echo "Perkalian 7 x 2 adalah " . perkalian(7, 2);
+?>
